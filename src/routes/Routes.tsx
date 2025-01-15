d5f23499cc0f7292f50d4ed722ea66d19ba63533
Md Rashedul Islam <rajib5570@gmail.com>
2025-01-15 21:50:10 +0600

    [add] : contact.tsx layout created

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router";
 import App from "../App";
+import About from "../pages/About";
 
 const AppRoutes = () => {
   return (
     <Routes>
           <Route path="/" element={<App />}>
-              <Route path="about" element={ } />
+              <Route path="about" element={<About /> } />
           </Route>
     </Routes>
   );
