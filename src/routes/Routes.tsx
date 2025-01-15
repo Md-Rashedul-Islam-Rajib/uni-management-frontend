fe7344eddcc436570ee35af4809db89252489c7d
Md Rashedul Islam <rajib5570@gmail.com>
2025-01-15 22:55:52 +0600

    [add] : admindashboard.tsx layout created

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import App from "../App";
-import About from "../pages/About";
+import About from "../pages/shared/About";
+import Contact from "../pages/shared/Contact";
 
 const AppRoutes = () => {
   return (
     <Routes>
           <Route path="/" element={<App />}>
               <Route path="about" element={<About /> } />
+              <Route path="contact" element={<Contact /> } />
           </Route>
     </Routes>
   );
