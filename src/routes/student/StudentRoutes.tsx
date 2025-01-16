42159c55161e620337a0874975f47e9acdc26106
Md Rashedul Islam <rajib5570@gmail.com>
2025-01-17 00:00:21 +0600

    [add ] : student routes added

---
+++ src/routes/student/StudentRoutes.tsx
@@ -0,0 +1,7 @@
+import StudentDashboard from "../../pages/student/StudentDashboard";
+
+export const studentPaths = [{
+    name: 'Dashboard',
+    path: "dashboard",
+    element: <StudentDashboard />
+}]
