faecc4330e71696c70032b1c11dd18a34a414c81
Md Rashedul Islam <rajib5570@gmail.com>
2025-01-19 14:12:15 +0600

    [add] : unauthorized page added

---
+++ src/pages/shared/Unauthorized.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Unauthorized = () => {
+  return (
+    <div>
+      You are Unauthorized for this route
+    </div>
+  )
+}
+
+export default Unauthorized
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,13 +6,18 @@ import { adminPaths } from "./admin/AdminRoutes";
 import { generateRoutes } from "../utilities/routeGenerator";
 import { studentPaths } from "./student/StudentRoutes";
 import ProtectedRoute from "../pages/shared/ProtectedRoute";
+import Login from "../pages/shared/Login";
+import Unauthorized from "../pages/shared/Unauthorized";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
+        <Route path="login" element={<Login/>} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
       </Route>
       <Route path="/admin" element={<ProtectedRoute allowedRoles={["admin"]} />}>
         {generateRoutes(adminPaths)}
