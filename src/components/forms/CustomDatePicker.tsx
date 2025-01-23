6d55052934baa8ffaea4bb0c1b7bee6ac6cdb0b5
Md Rashedul Islam <rajib5570@gmail.com>
2025-01-23 21:04:42 +0600

    [add] : custom date picker added

--- src/components/forms/CustomDatePicker.tsx
+++ src/components/forms/CustomDatePicker.tsx
@@ -1,8 +1,19 @@
+import { Controller } from "react-hook-form"
+import { TDatePickerProps } from "../../types/form.types"
+import { DatePicker, Form } from "antd"
 
 
-const CustomDatePicker = () => {
+const CustomDatePicker = ({name,label}: TDatePickerProps) => {
   return (
-    <div>
+      <div style={{ marginBottom: '20px' }}>
+          <Controller
+          name={name}
+              render={({ field }) => (
+                  <Form.Item label={label}>
+                      <DatePicker {...field} size="large" style ={{width: '100%'}} />
+              </Form.Item>
+          )}
+          />
       
     </div>
   )
